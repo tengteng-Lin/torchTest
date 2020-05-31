3dd2dbf1d0d320e44605fc6110b51de525420dd5
tengteng_Lin <nanyi_Gu@163.com>
2020-05-31 09:49:19 +0800

    test_0

--- test_0.py
+++ test_0.py
@@ -16,6 +16,25 @@ print(x)
 x = x.new_ones(5,3,dtype=torch.double)
 print(x)
 
-x = torch.rand_like(x,dtype=torch.float)
+x = torch.rand_like(x,dtype=torch.double)   #float是CPU用的，GPU用double
 print(x) #维度一致
 print(x.size()) #是一个元组
+
+y = torch.rand(5,3,dtype=torch.double)
+
+print(x+y)
+result = torch.zeros(5,3,dtype=torch.double) #GPU版本必须有dtype
+torch.add(x,y,out=result)
+print(result)
+
+y.add_(x) #add x to y
+print(y[:,1]) #第一列（从第0列开始数）
+print(y[1,:]) #第一行
+
+x = torch.rand(4,4)
+y = x.view(1,16) #重构大小
+z = x.view(-1,8)
+print(x,y,z)
+
+x = torch.rand(2) #1x2
+print(torch.rand(1).item()) #只有1x1有
